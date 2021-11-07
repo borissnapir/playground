d24d888c8f93d65d1996bf30297a22882f886d6d
Boris Snapir <snapir.boris@gmail.com>
2021-11-07 22:25:26 +0100

    jpeg classifier as cloudfree or cloudy

---
+++ simple_image_classifier/.ipynb_checkpoints/CNN-checkpoint.py
@@ -0,0 +1,20 @@
+import tensorflow as tf
+
+
+class CNN():
+    def __init__(self):
+        self.build_model()
+
+
+    def build_model(self):
+        model = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu', input_shape=(64,64,3)),
+            tf.keras.layers.MaxPooling2D(pool_size=2),
+            tf.keras.layers.Dropout(0.3),
+            tf.keras.layers.Conv2D(filters=16, kernel_size=3, padding='valid', activation='relu'),
+            tf.keras.layers.MaxPooling2D(pool_size=2),
+            tf.keras.layers.Dropout(0.3),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(1, activation='sigmoid'), # No activation on final dense layer
+        ])
+        self.model = model
