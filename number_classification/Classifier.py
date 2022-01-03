0e6cf000507f2a7c86fca1366eea08fba3a6fe38
Boris Snapir <snapir.boris@gmail.com>
2022-01-03 22:07:11 +0100

    jersey classification

--- number_classification/Classifier.py
+++ number_classification/Classifier.py
@@ -7,7 +7,7 @@ class Classifier():
 
 
     def build_model(self):
-        model = tf.keras.Sequential([
+        model_seq = tf.keras.Sequential([
             tf.keras.layers.Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu', input_shape=(64,64,3)),
             tf.keras.layers.MaxPooling2D(pool_size=2),
             tf.keras.layers.Dropout(0.3),
@@ -15,6 +15,9 @@ class Classifier():
             tf.keras.layers.MaxPooling2D(pool_size=2),
             tf.keras.layers.Dropout(0.3),
             tf.keras.layers.Flatten(),
-            tf.keras.layers.Dense(100, activation='softmax'), # 00-99
+#             tf.keras.layers.Dense(100, activation='softmax'), # 00-99
         ])
+        first_digit_output = tf.keras.layers.Dense(10, activation="softmax")(model_seq.output)
+        second_digit_output = tf.keras.layers.Dense(10, activation="softmax")(model_seq.output)
+        model = tf.keras.Model(inputs=model_seq.input, outputs=[first_digit_output, second_digit_output])
         self.model = model
